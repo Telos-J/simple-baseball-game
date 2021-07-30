de0870e5ff8e8b71547b4ff11726f1e22753dcc5
Jiseung Jung <roadrunner23@yonsei.ac.kr>
2021-07-30 11:12:00 +0900

    format layout

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const canvas = document.querySelector('canvas')
 const context = canvas.getContext('2d')
-console.log(canvas.width, canvas.height)
-context.fillStyle = 'turquoise'
-context.fillRect(0, 0, 300, 100)
+
+canvas.width = innerWidth * devicePixelRatio
+canvas.height = innerHeight * devicePixelRatio
+context.fillStyle = 'white'
+context.fillRect(0, 0, canvas.width, canvas.height)
