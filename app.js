7e36533199662cc99dd3db3702c44e73c85acd40
SJ100810 <seungjune.yoon@gmail.com>
2021-07-30 10:36:23 +0800

    fixed something

--- app.js
+++ app.js
@@ -3,5 +3,20 @@ const context = canvas.getContext('2d')
 
 canvas.width = innerWidth * devicePixelRatio
 canvas.height = innerHeight * devicePixelRatio
-context.fillStyle = 'white'
-context.fillRect(0, 0, canvas.width, canvas.height)
+
+let x = 300 * Math.random()
+let y = 150 * Math.random()
+
+function loop() {
+    context.fillStyle = 'white'
+    context.fillRect(0, 0, canvas.width, canvas.height)
+    x += 50 * Math.random()
+    y += 50 * Math.random()
+    context.strokeStyle = 'black'
+    context.beginPath()
+    context.arc(x, y, 20, 0, Math.PI * 2)
+    context.stroke()        
+    requestAnimationFrame(loop) // recursive function
+}
+
+loop()
