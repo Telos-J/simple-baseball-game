463fa961c8a4d182540e4fc9e74c985aa82eef97
Jiseung Jung <roadrunner23@yonsei.ac.kr>
2021-08-12 10:59:49 +0900

    some fixes

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ class Ball {
             setTimeout(() => {
                 this.x = canvas.width / 2 - this.size / 2
                 this.y = 366
+                camera.x = 0
                 camera.y = 0
                 pitched = false
             }, 2*1000)
@@ -94,13 +95,16 @@ class Bat {
 
         if (this.rotationSpeed !== 0 && ball.y > 760 && ball.y < 780 && this.rotation > Math.PI - Math.PI && this.rotation < Math.PI - Math.PI / 1.5) {
             ball.speed = 6*Math.random() + 42
-            ball.rotation = bat.rotation + Math.PI * 25 / 18 
+            ball.rotation = this.rotation + Math.PI * 25 / 18 
+            console.log(this.rotation, ball.rotation)
         } else if (this.rotationSpeed !== 0 && ball.y > 700 && ball.y < 850 && this.rotation > Math.PI - Math.PI && this.rotation < Math.PI - Math.PI / 1.5) {
-            bat.speed = 20*Math.random() + 15 
-            ball.rotation = bat.rotation + Math.PI * 25 / 18 
+            ball.speed = 20*Math.random() + 15 
+            ball.rotation = this.rotation + Math.PI * 25 / 18 
+            console.log(this.rotation, ball.rotation)
         } else if (this.rotationSpeed !== 0 && ball.y > 670 && ball.y < 860 && this.rotation > Math.PI - Math.PI && this.rotation < Math.PI - Math.PI / 1.5) {
-            bat.speed = 10*Math.random() + 10
-            ball.rotation = bat.rotation + Math.PI * 25 / 18 
+            ball.speed = 10*Math.random() + 10
+            ball.rotation = this.rotation + Math.PI * 25 / 18 
+            console.log(this.rotation, ball.rotation)
         } 
     }
 
@@ -152,7 +156,7 @@ addEventListener('keydown', e => {
         if (inningSituation === true) {
             ball.vy = 15
         } else {
-            bat.rotationSpeed = Math.PI /  5
+            bat.rotationSpeed = Math.PI /  10
         }
     } else if (e.code === 'ArrowUp') {s
         camera.y -= 10
@@ -174,6 +178,7 @@ setInterval(() => {
     if (inningSituation === false && ball.vy === 0 && pitched === false) {
         pitched = true
         ball.speed = 15*Math.random() + 5
+        ball.rotation = Math.PI / 2
         ball.timeoutset = false
     }
 },3*1000)
